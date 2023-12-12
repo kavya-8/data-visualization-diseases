eaa939fe2f88a92e95a4dd2e99e331b412fa19a7
SohaSk <150521718+SohaSk@users.noreply.github.com>
2023-12-12 18:10:50 +0530

    Add files via upload

---
+++ DiseasesInfo/static/search.js
@@ -0,0 +1,2 @@
+var value = document.getElementById("SearchInput").value;
+localStorage.setItem("searchValue", value);
---
+++ DiseasesInfo/static/signup.js
@@ -0,0 +1,10 @@
+let signup = document.querySelector(".signup");
+
+let formSection = document.querySelector(".form-section");
+
+
+signup.addEventListener("click", () => {
+
+
+    formSection.classList.add("form-section-move");
+});
